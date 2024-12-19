88f9fbf9887e4df88beb92fd79fb5cb06aba79b1
Hopeudoka <wisparex02@gmail.com>
2024-12-19 23:40:31 +0100

    complete week 6 learning activity

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        Square newSquare = new Square("Green", 10.0);
+
+        // Console.WriteLine($"{newSquare.GetColor()}");
+        // Console.WriteLine($"{newSquare.GetArea()}");
+
+        Rectangle newRectangle = new Rectangle("Grey", 15, 10);
+
+        // Console.WriteLine($"{newRectangle.GetColor()}");
+        // Console.WriteLine($"{newRectangle.GetArea()}"); 
+
+        Circle newCircle = new Circle("Red", 4);
+
+        // Console.WriteLine($"{newCircle.GetColor()}");
+        // Console.WriteLine($"{newCircle.GetArea()}");
+
+        List<Shape> shapeList = new List<Shape>();
+        
+        shapeList.Add(newSquare);
+        shapeList.Add(newRectangle);
+        shapeList.Add(newCircle);
+
+        foreach (Shape shape in shapeList)
+        {
+            Console.WriteLine($"{shape.GetColor()}");
+            Console.WriteLine($"{shape.GetArea()}");
+        }
     }
 }
