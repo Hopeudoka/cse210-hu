65c97372a4f0f5c109d9cbe9e546d0d3364f0cc4
Hopeudoka <wisparex02@gmail.com>
2024-12-21 00:41:08 +0100

    week 7 foundation completed

---
+++ foundation/Foundation3/Cycling.cs
@@ -0,0 +1,24 @@
+public class Cycling : Activity
+{
+    private double _speed; // Speed in mph
+
+    public Cycling(DateTime date, int minutes, double speed) : base(date, minutes)
+    {
+        _speed = speed;
+    }
+
+    public override double GetDistance()
+    {
+        return (_speed * Minutes) / 60;
+    }
+
+    public override double GetSpeed()
+    {
+        return _speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / _speed;
+    }
+}
--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        // Create activities
+        Running running = new Running(new DateTime(2022, 11, 3), 30, 3.0);
+        Cycling cycling = new Cycling(new DateTime(2022, 11, 3), 30, 15.0);
+        Swimming swimming = new Swimming(new DateTime(2022, 11, 3), 30, 20);
+
+        // Add activities to a list
+        List<Activity> activities = new List<Activity> { running, cycling, swimming };
+
+        // Display summaries
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
